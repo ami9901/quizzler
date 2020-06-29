293762822df4a18d7be5c39f447b1ce05292ece2
AMISHA NARSINGANI <amishanarsingani@gmail.com>
2020-06-29 22:09:23 +0530

    creating a question class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'question.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,12 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+
+  List<Question> questionBank = [
+    Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
+    Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
+    Question(q: 'A slug\'s blood is green.', a: true)
   ];
-  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
@@ -47,7 +48,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questions[questionNumber],
+                questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,11 +72,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer=answers[questionNumber];
-                if(correctAnswer==true){
+                bool correctAnswer = questionBank[questionNumber].answerValue;
+                if (correctAnswer == true) {
                   print('user got it right');
-                }
-                else{
+                } else {
                   print('user got it wrong');
                 }
                 setState(() {
@@ -99,11 +99,10 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                bool correctAnswer=answers[questionNumber];
-                if(correctAnswer==false){
+                bool correctAnswer = questionBank[questionNumber].answerValue;
+                if (correctAnswer == false) {
                   print('user got it right');
-                }
-                else{
+                } else {
                   print('user got it wrong');
                 }
                 setState(() {
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+class Question{
+  String questionText;
+  bool answerValue;
+
+  Question({String q,bool a}){
+    questionText=q;
+    answerValue=a;
+  }
+}
